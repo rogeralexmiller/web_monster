25b1cf646e5094c87000c20f9160529e516e3ae5
Roger Miller <rogeralexmiller@gmail.com>
2018-01-05 02:25:01 -0500

    debug tests

--- test/controllers/api/web_contents_controller_test.rb
+++ test/controllers/api/web_contents_controller_test.rb
@@ -12,10 +12,16 @@ class Api::WebContentsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:web_contents)
   end
 
-  test "should create web_content" do
+  test "should create web_content from a url" do
     assert_difference('WebContent.count', 1) do
       form = { url: "https://www.facebook.com" }
       post "create", "api/web_contents", form, format: 'json'
     end
+  end
+
+  test "should return 400 when invalid or non-existent url is passed" do
+    form = { url: "https://www.fakewebsite3838429sljdlkfjlsflskdla" }
+    post "create", "api/web_contents", form, format: 'json'
+    assert_response 400
   end
 end
