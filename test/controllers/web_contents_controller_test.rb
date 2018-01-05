c3236a1be50016fb530950ebf08de508757e9d40
Roger Miller <rogeralexmiller@gmail.com>
2018-01-04 23:40:33 -0500

    debug model and controller tests

--- test/controllers/web_contents_controller_test.rb
+++ test/controllers/web_contents_controller_test.rb
@@ -11,39 +11,11 @@ class WebContentsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:web_contents)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create web_content" do
     assert_difference('WebContent.count') do
-      post :create, web_content: {  }
+      post :create, web_content: { url: "https://facebook.com", content: "faces faces everywhere"  }
     end
 
-    assert_redirected_to web_content_path(assigns(:web_content))
-  end
-
-  test "should show web_content" do
-    get :show, id: @web_content
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @web_content
-    assert_response :success
-  end
-
-  test "should update web_content" do
-    patch :update, id: @web_content, web_content: {  }
-    assert_redirected_to web_content_path(assigns(:web_content))
-  end
-
-  test "should destroy web_content" do
-    assert_difference('WebContent.count', -1) do
-      delete :destroy, id: @web_content
-    end
-
-    assert_redirected_to web_contents_path
+    assert_redirected_to api_web_content_path(assigns(:web_content))
   end
 end
--- test/models/web_content_test.rb
+++ test/models/web_content_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class WebContentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @null_url = web_contents(:null_url)
+    @bad_url = web_contents(:bad_url)
+    @good_url = web_contents(:one)
+  end
+
+  test "should not save without a url" do
+    assert_not @null_url.save
+  end
+
+  test "should not save a malformed url" do
+    assert_not @bad_url.save
+  end
+
+  test "should save a good url" do
+    assert @good_url.save
+  end
 end
