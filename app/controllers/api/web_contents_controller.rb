a57c54df49206bca39e4a3a81a04bdb568f9df9f
Roger Miller <rogeralexmiller@gmail.com>
2018-01-08 01:04:55 -0500

    switch to using httparty to handle status codes

--- app/controllers/api/web_contents_controller.rb
+++ app/controllers/api/web_contents_controller.rb
@@ -1,6 +1,4 @@
 class Api::WebContentsController < ApplicationController
-  require 'open-uri'
-  require 'net/http'
 
   before_action :set_variables, :verify_url, only: [:create]
 
@@ -9,54 +7,109 @@ class Api::WebContentsController < ApplicationController
   end
 
   def create
-    @response = Net::HTTP.get_response(URI(@url))
+    index_url
 
-    if successful_response?
+    if @success
       render json: { message: "success" }, status: 200
     else
-      error_response
+      render json: { message: @message }, status: 400
     end
 
-  rescue
-    error_response
+  rescue => e
+    Rails.logger.fatal("Crashed parsing url: #{@url}")
+    Rails.logger.fatal(e)
+    render json: { message: "Unknown error occurred. Unable to parse url." },
+           status: 400
   end
 
   private
 
-  def error_response
-    render json: { message: "Couldn't fetch or parse web page." }, status: 400
+  def index_url
+    @response = HTTParty.get(@url)
+
+    handle_response
+
+  rescue HTTParty::Error
+    @success = false
+    @message = "Unknown error occurred. Unable to fetch url"
   end
 
-  def save_content
-    WebContent.new(url: @url, content: encoded_content).save
-  end
-
-  def encoded_content
-    @content.force_encoding('UTF-8').encode('UTF-8', invalid: :replace,
-                                                     undef: :replace,
-                                                     replace: ' ')
+  def handle_response
+    case @response.code
+      when 200
+        parse_response
+      when 404
+        @success = false
+        @message = "No content found at that url."
+      when 500...600
+        @success = false
+        @message = "Url responding with server error."
+    end
   end
 
   def parse_response
-    tag_collections.each do |collection|
-      parse_elements(collection)
-    end
-  end
-
-  def parse_elements(elements)
-    elements.each do |tag|
-      @content += tag.text + " "
-    end
-  end
-
-  def tag_collections
     page = Nokogiri::HTML(@response.body)
 
-    [page.css('h1'), page.css('h2'), page.css('h3'), page.css('a')]
+    parse_header_groups(page)
+    parse_links(page)
+
+    encode_content
+
+    save_content
+
+  rescue => e
+    Rails.logger.error("Failed to parse content at url: #{@url}")
+    Rails.logger.error(e)
+    @success = false
+    @message = "Unable to parse content at url."
   end
 
-  def successful_response?
-    @response.is_a?(Net::HTTPSuccess) && parse_response && save_content
+  def save_content
+    WebContent.new(url: @url, content: @content).save
+  end
+
+  def encode_content
+    @content = @content.encode('UTF-8', invalid: :replace, undef: :replace,
+                                        replace: ' ')
+  end
+
+  def parse_header_groups(page)
+    header_groups = [page.css('h1'), page.css('h2'), page.css('h3')]
+
+    header_groups.each do |headers|
+      parse_headers(headers)
+    end
+  end
+
+  def parse_links(page)
+    link_tags = page.css('a')
+
+    link_tags.each do |link|
+      @content += tag_content(link)
+      @content += link_href(link)
+    end
+  end
+
+  def parse_headers(headers)
+    headers.each do |tag|
+      @content += tag_content(tag)
+    end
+  end
+
+  def tag_content(tag)
+    if tag and tag.text
+      tag.text + " "
+    else
+      ''
+    end
+  end
+
+  def link_href(link)
+    if link && link['href']
+      link['href'] + " "
+    else
+      ''
+    end
   end
 
   def verify_url
@@ -67,6 +120,7 @@ class Api::WebContentsController < ApplicationController
   end
 
   def set_variables
+    @success = true
     @content = ""
     @url = params[:url] || ""
   end
